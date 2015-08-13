ba92c85f70227c415f29609b3bbc1fbb3fe81213
Christian Scheb <info@stylemotion.de>
2015-08-13 23:27:16 +0200

    Compact log format

--- src/Formatter/LogFormatter.php
+++ src/Formatter/LogFormatter.php
@@ -1,7 +1,21 @@
 <?php
 namespace Scheb\Tombstone\Formatter;
 
-class LogFormatter extends JsonFormatter
+use Scheb\Tombstone\Logging\LogFormat;
+use Scheb\Tombstone\Vampire;
+
+class LogFormatter implements FormatterInterface
 {
 
+    /**
+     * Formats a Vampire for the log
+     *
+     * @param Vampire $vampire
+     *
+     * @return string
+     */
+    public function format(Vampire $vampire)
+    {
+        return LogFormat::vampireToLog($vampire) . "\n";
+    }
 }
---
+++ src/Logging/LogFormat.php
@@ -0,0 +1,37 @@
+<?php
+namespace Scheb\Tombstone\Logging;
+
+use Scheb\Tombstone\Vampire;
+
+class LogFormat
+{
+    /**
+     * @param Vampire $vampire
+     *
+     * @return string
+     */
+    public static function vampireToLog(Vampire $vampire) {
+        return $vampire->getAwakeningDate() . "\t"
+            . $vampire->getTombstoneDate() . "\t"
+            . $vampire->getAuthor() . "\t"
+            . $vampire->getFileName() . "\t"
+            . $vampire->getLine() . "\t"
+            . $vampire->getMethod() . "\t"
+            . $vampire->getInvoker();
+    }
+
+    /***
+     * @param string $log
+     *
+     * @return Vampire|null Returns null when log was not valid
+     */
+    public static function logToVampire($log)
+    {
+        $v = explode("\t", $log);
+        if (count($v) !== 7) {
+            return null;
+        }
+
+        return new Vampire($v[0], $v[1], $v[2], $v[3], $v[4], $v[5], $v[6]);
+    }
+}
