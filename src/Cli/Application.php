9ccea8f6fd14b1020ecd219c77f0d1a8a30acec8
Christian Scheb <info@stylemotion.de>
2015-08-16 00:19:25 +0200

    CLI added

---
+++ src/Cli/Application.php
@@ -0,0 +1,42 @@
+<?php
+namespace Scheb\Tombstone\Cli;
+
+use Symfony\Component\Console\Application as AbstractApplication;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Application extends AbstractApplication
+{
+    public function __construct()
+    {
+        AbstractApplication::__construct('cli');
+    }
+
+    protected function getCommandName(InputInterface $input)
+    {
+        return 'analyze';
+    }
+
+    protected function getDefaultCommands()
+    {
+        $defaultCommands = AbstractApplication::getDefaultCommands();
+        $defaultCommands[] = new Command;
+        return $defaultCommands;
+    }
+
+    public function getDefinition()
+    {
+        $inputDefinition = AbstractApplication::getDefinition();
+        $inputDefinition->setArguments();
+        return $inputDefinition;
+    }
+
+    public function doRun(InputInterface $input, OutputInterface $output)
+    {
+        if (!$input->getFirstArgument()) {
+            $input = new ArrayInput(array('--help'));
+        }
+        AbstractApplication::doRun($input, $output);
+    }
+}
