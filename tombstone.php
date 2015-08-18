cd458050483babd131c4dfa401f1eaf5e5a72817
Christian Scheb <info@stylemotion.de>
2015-08-18 23:18:10 +0200

    PHPDoc

--- tombstone.php
+++ tombstone.php
@@ -1,6 +1,12 @@
 <?php
 
 if (!function_exists('tombstone')) {
+
+    /**
+     * @param string $date Any date format strtotime() understands
+     * @param string $author Your name
+     * @param string|null $label An optional label for the tombstone
+     */
     function tombstone($date, $author, $label = null) {
         try {
             $trace = \Scheb\Tombstone\Tracing\TraceProvider::getTraceHere();
