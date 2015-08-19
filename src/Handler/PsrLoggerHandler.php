d9ed04f7421c6aec59bbee7b4f1797eec19e762f
Christian Scheb <info@stylemotion.de>
2015-08-19 22:34:54 +0200

    Added PsrLoggerHandler

---
+++ src/Handler/PsrLoggerHandler.php
@@ -0,0 +1,38 @@
+<?php
+namespace Scheb\Tombstone\Handler;
+
+use Psr\Log\LoggerInterface;
+use Scheb\Tombstone\Vampire;
+
+class PsrLoggerHandler extends AbstractHandler
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var mixed
+     */
+    private $level;
+
+    /**
+     * @param LoggerInterface $logger
+     * @param mixed $level
+     */
+    public function __construct(LoggerInterface $logger, $level)
+    {
+        $this->logger = $logger;
+        $this->level = $level;
+    }
+
+    /**
+     * Log a vampire
+     *
+     * @param Vampire $vampire
+     */
+    public function log(Vampire $vampire)
+    {
+        $this->logger->log($this->level, $this->getFormatter()->format($vampire));
+    }
+}
