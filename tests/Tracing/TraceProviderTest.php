ccf1952bad9df78c9ac0188f64b939fb7564c7e1
Christian Scheb <info@stylemotion.de>
2015-08-20 21:01:55 +0200

    Test for tracer

---
+++ tests/Tracing/TraceProviderTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Scheb\Tombstone\Tests\Tracing;
+
+use Scheb\Tombstone\Tracing\TraceProvider;
+
+class TraceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function getTraceHere_traceContainingFunction_returnStackTrace() {
+        $thisMethod = __FUNCTION__;
+        $thisClass = __CLASS__;
+
+        $trace = TraceProvider::getTraceHere();
+        $this->assertInternalType('array', $trace);
+        $this->assertGreaterThanOrEqual(3, $trace);
+        $this->assertEquals($thisMethod, $trace[0]['function']);
+        $this->assertEquals($thisClass, $trace[0]['class']);
+    }
+}
