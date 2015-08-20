4ab507eebf29fb88c69d8e4c62888bf1a50b2c6d
Christian Scheb <info@stylemotion.de>
2015-08-20 22:00:47 +0200

    Handler test

---
+++ tests/Handler/PsrLoggerHandlerTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Scheb\Tombstone\Tests\Handler;
+
+use Scheb\Tombstone\Handler\PsrLoggerHandler;
+use Scheb\Tombstone\Tests\Fixtures\LabelFormatter;
+use Scheb\Tombstone\Tests\Fixtures\VampireFixture;
+
+class PsrLoggerHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function log()
+    {
+        $logger = $this->getMock('Psr\Log\LoggerInterface');
+        $logger
+            ->expects($this->once())
+            ->method('log')
+            ->with('level', 'label');
+
+        $handler = new PsrLoggerHandler($logger, 'level');
+        $handler->setFormatter(new LabelFormatter());
+
+        $handler->log(VampireFixture::getVampire());
+    }
+}
