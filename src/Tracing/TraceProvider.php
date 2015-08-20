741f5c72ad8bcc458a87743d1594b44944c58d7d
Christian Scheb <info@stylemotion.de>
2015-08-20 21:26:44 +0200

    PHP 5.3 compatibility

--- src/Tracing/TraceProvider.php
+++ src/Tracing/TraceProvider.php
@@ -10,7 +10,7 @@ class TraceProvider {
      */
     public static function getTraceHere($skipFrames = 0) {
         ++$skipFrames; // Skip this call
-        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $skipFrames + 3);
-        return array_splice($trace, $skipFrames);
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        return array_splice($trace, $skipFrames, 3);
     }
 }
