a3b27b45182f670b8f61db0aa21c99b5eac2e22d
Christian Scheb <info@stylemotion.de>
2018-12-06 23:27:45 +0100

    Use exception to get the trace

--- src/Tracing/TraceProvider.php
+++ src/Tracing/TraceProvider.php
@@ -7,7 +7,7 @@ class TraceProvider
     public static function getTraceHere(int $skipFrames = 0): array
     {
         ++$skipFrames; // Skip this call
-        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        $trace = (new \Exception())->getTrace();
 
         return array_splice($trace, $skipFrames, 3);
     }
