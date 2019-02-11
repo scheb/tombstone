620f3326a1b83259cc8f18d04b61cd74d086314a
Christian Scheb <info@stylemotion.de>
2019-02-11 23:42:29 +0100

    Add a custom provided vampire

--- tests/_application/src/Tombstone/LogProvider.php
+++ tests/_application/src/Tombstone/LogProvider.php
@@ -4,10 +4,15 @@ namespace Scheb\Tombstone\Analyzer\TestApplication\Tombstone;
 
 use Scheb\Tombstone\Analyzer\Log\LogReaderInterface;
 use Scheb\Tombstone\Analyzer\VampireIndex;
+use Scheb\Tombstone\Tracing\TraceProvider;
+use Scheb\Tombstone\Vampire;
 
 class LogProvider implements LogReaderInterface
 {
     public function collectVampires(VampireIndex $vampireIndex): void
     {
+        $trace = TraceProvider::getTraceHere(0);
+        $vampire = Vampire::createFromCall(['customProvided'], $trace, []);
+        $vampireIndex->addVampire($vampire);
     }
 }
