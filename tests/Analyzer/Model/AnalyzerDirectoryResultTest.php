06dbcaad88edfb80cfcbf27a4704d2965cf389bf
Christian Scheb <privat@stylemotion.de>
2020-08-07 14:56:24 +0200

    Add test cases

--- tests/Analyzer/Model/AnalyzerDirectoryResultTest.php
+++ tests/Analyzer/Model/AnalyzerDirectoryResultTest.php
@@ -5,10 +5,42 @@ declare(strict_types=1);
 namespace Scheb\Tombstone\Tests\Analyzer\Model;
 
 use Scheb\Tombstone\Analyzer\Model\AnalyzerDirectoryResult;
+use Scheb\Tombstone\Analyzer\Model\AnalyzerFileResult;
 use Scheb\Tombstone\Tests\TestCase;
 
 class AnalyzerDirectoryResultTest extends TestCase
 {
+    private function createDirectoryResult(int $deadCount, int $undeadCount, int $deletedCount): AnalyzerDirectoryResult
+    {
+        $result = $this->createMock(AnalyzerDirectoryResult::class);
+        $result->expects($this->any())->method('getDeadCount')->willReturn($deadCount);
+        $result->expects($this->any())->method('getUndeadCount')->willReturn($undeadCount);
+        $result->expects($this->any())->method('getDeletedCount')->willReturn($deletedCount);
+
+        return $result;
+    }
+
+    private function createFileResult(int $deadCount, int $undeadCount, int $deletedCount): AnalyzerFileResult
+    {
+        $result = $this->createMock(AnalyzerFileResult::class);
+        $result->expects($this->any())->method('getDeadCount')->willReturn($deadCount);
+        $result->expects($this->any())->method('getUndeadCount')->willReturn($undeadCount);
+        $result->expects($this->any())->method('getDeletedCount')->willReturn($deletedCount);
+
+        return $result;
+    }
+
+    private function createAnalyzerDirectoryResult(): AnalyzerDirectoryResult
+    {
+        $directoryResult1 = $this->createDirectoryResult(1, 1, 1);
+        $directoryResult2 = $this->createDirectoryResult(0, 0, 1);
+
+        $fileResult1 = $this->createFileResult(0, 2, 0);
+        $fileResult2 = $this->createFileResult(3, 0, 0);
+
+        return new AnalyzerDirectoryResult('dir', [$directoryResult1, $directoryResult2], [$fileResult1, $fileResult2]);
+    }
+
     /**
      * @test
      */
@@ -34,5 +66,32 @@ class AnalyzerDirectoryResultTest extends TestCase
     {
         $result = new AnalyzerDirectoryResult('', [], []);
         $this->assertEquals('', $result->getDirectoryName());
+    }
+
+    /**
+     * @test
+     */
+    public function getDeadCount_directoryAndFileResultsGiven_returnSum(): void
+    {
+        $result = $this->createAnalyzerDirectoryResult();
+        $this->assertEquals(4, $result->getDeadCount());
+    }
+
+    /**
+     * @test
+     */
+    public function getUndeadCount_directoryAndFileResultsGiven_returnSum(): void
+    {
+        $result = $this->createAnalyzerDirectoryResult();
+        $this->assertEquals(3, $result->getUndeadCount());
+    }
+
+    /**
+     * @test
+     */
+    public function getDeletedCount_directoryAndFileResultsGiven_returnSum(): void
+    {
+        $result = $this->createAnalyzerDirectoryResult();
+        $this->assertEquals(2, $result->getDeletedCount());
     }
 }
