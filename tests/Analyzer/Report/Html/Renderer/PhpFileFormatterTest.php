459001b26dd25f5390bee4dd4f47c8a2c621dd8a
Christian Scheb <privat@stylemotion.de>
2020-12-08 18:43:27 +0100

    Add expected formatting for PHP8

--- tests/Analyzer/Report/Html/Renderer/PhpFileFormatterTest.php
+++ tests/Analyzer/Report/Html/Renderer/PhpFileFormatterTest.php
@@ -10,15 +10,23 @@ use Scheb\Tombstone\Tests\TestCase;
 
 class PhpFileFormatterTest extends TestCase
 {
+    private const EXPECTED_PHP7 = __DIR__.'/fixtures/FormattingTestClass.html';
+    private const EXPECTED_PHP8 = __DIR__.'/fixtures/FormattingTestClassPhp8.html';
+
     /**
      * @test
      */
     public function loadFile_phpFileGiven_formattedHtmlReturned()
     {
+        $expectedFile = self::EXPECTED_PHP7;
+        if (PHP_MAJOR_VERSION >= 8) {
+            $expectedFile = self::EXPECTED_PHP8;
+        }
+
         $formatter = new PhpFileFormatter(new PhpSyntaxHighlighter());
         $formattedLines = $formatter->formatFile(__DIR__.'/fixtures/FormattingTestClass.php');
         $formattedFile = implode("<br />\n", $formattedLines);
         file_put_contents(__DIR__.'/fixtures/FormattingTestClass.actual.html', $formattedFile);
-        $this->assertStringEqualsFile(__DIR__.'/fixtures/FormattingTestClass.html', $formattedFile);
+        $this->assertStringEqualsFile($expectedFile, $formattedFile);
     }
 }
