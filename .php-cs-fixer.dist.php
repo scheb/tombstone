00a30fe3d613015cbdb1a9fab84ff73916888df1
Christian Scheb <privat@stylemotion.de>
2021-05-12 22:58:08 +0200

    Drop support for php-cs-fixer v2

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,3 +1,28 @@
 <?php
 
-return require '.php_cs.dist';
+$rules = [
+    '@Symfony' => true,
+    'native_function_invocation' => ['include' => ['@compiler_optimized'], 'scope' => 'namespaced'],
+    'phpdoc_to_comment' => false,
+    'phpdoc_align' => false,
+    'php_unit_method_casing' => false,
+];
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->notPath('FormattingTestClass.php')
+    ->notPath('function_names.php')
+;
+
+$config = new PhpCsFixer\Config();
+$config
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+    ->setRules($rules)
+    ->setUsingCache(true)
+;
+
+return $config;
